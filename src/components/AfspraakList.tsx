110c7096e49b886e84a6d9342a96ffa70a864457
DieterDraelants <dieter.draelants@student.ucll.be>
2020-11-05 16:09:55 +0100

    styling task list

--- src/components/AfspraakList.tsx
+++ src/components/AfspraakList.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, StyleSheet} from 'react-native'
 import { Context as AfspraakContext } from '../context/AfspraakContext'
 import { deleteAfspraak, updateAfspraak } from '../actions/AfspraakActions'
 import { ToolbarAndroidBase } from 'react-native'
@@ -22,21 +22,54 @@ export const AfspraakList: React.FC = () => {
             data={afspraken} 
             keyExtractor={item => item.id} 
             renderItem={({item}) => 
-                <View>
-                    <Text>{item.title}</Text>
+                <View style={styles.TaskContainer}>
+                    <Text style={styles.title}>{item.title}</Text>
                     <Text>{item.description}</Text>
-                    <Button title="Delete" onPress={() => deleteHandler(item.id)}></Button>
-                    <Button title="Update" onPress={() => navigation.navigate('updateAfspraak')}></Button>
+                    <View style={styles.buttonGroup}>
+                        <Button title="Delete" onPress={() => deleteHandler(item.id)}></Button>
+                        <Button title="Update" onPress={() => navigation.navigate('updateAfspraak')}></Button>
+                    </View>
                 </View>
                 }>
         </FlatList>
         </View>
     ) : (
-        <View>
-            <Text>Geen afspraken gevonden</Text>
+        <View style={styles.nothingFoundContainer}>
+            <Text style={styles.title}>Geen afspraken gevonden</Text>
         </View>
     )
 
 }
 
+
+const styles = StyleSheet.create({
+
+    nothingFoundContainer: {
+        marginTop: 25,
+        alignItems: "center",
+        display: "flex",
+        textAlign:"left",
+    },
+    TaskContainer: {
+        marginTop: 20,
+        marginBottom: 25,
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        marginHorizontal: 20
+    },
+    title: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+    buttonGroup: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: 135,
+        marginTop: 10,
+    },
+
+
+})
 export default AfspraakList
